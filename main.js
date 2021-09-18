d5043fdcb632749b1aad677ab1b5b538c66d3481
Ivana <ivana.sosacordero@innew.la>
2021-09-18 10:53:41 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.work', { delay: 200 });
+ScrollReveal().reveal('.beat', { delay: 200 });
+ScrollReveal().reveal('.instagram', { delay: 300 });
+ScrollReveal().reveal('.telegram', { delay: 600 });
